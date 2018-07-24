a3878d5776638e7b79d4638c4c53ad37787c07e7
Zahra Saeed Abdulla Hasan Abdulla Albasri <zahraa_albasri@yahoo.com>
2018-07-24 17:39:51 +0000

    links for channels don't work

--- application.py
+++ application.py
@@ -15,6 +15,8 @@ channel_list = {'general':[]}
 # temporary list
 msg_list = []
 
+current_page = "general"
+
 @app.route("/")
 def index():
     return render_template('index.html', channel_list=channel_list)
@@ -29,12 +31,40 @@ def channel(data):
 @socketio.on("submit message")
 def message(data):
     text = data["message"]
+    channel = data["channel"]
     t = time.asctime( time.localtime(time.time()) )
     message = text + " (" + str(t) + ")"
-    msg_list = channel_list['general']
+    msg_list = channel_list[channel]
     msg_list.append(message)
     while len(msg_list) > 100:
         del msg_list[0]
-    channel_list['general'] = msg_list
+    channel_list[channel] = msg_list
     emit("announce message", {"message": message}, broadcast=True)
 
+
+
+@socketio.on("current page")
+def page(data):
+    page = data["page"]
+    current_page = page
+    emit("current channel", {"current_page": current_page}, broadcast=True)
+
+
+@app.route("/messages", methods=["POST"])
+def messages():
+
+    # Get start and end point for posts to generate.
+    start = int(request.form.get("start") or 0)
+    end = int(request.form.get("end") or (start + 9))
+
+    # Generate list of posts.
+    data = []
+    for i in range(start, end + 1):
+        if len(channel_list[page]) >= i:
+            data.append(channel_list[page][i])
+
+    # Artificially delay speed of response.
+    time.sleep(1)
+
+    # Return list of posts.
+    return jsonify(data)
